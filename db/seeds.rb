7a1ba9360c19b7251eeec9f27c210182fc02597f
Bennis Yiu <bennisyiu@gmail.com>
2021-09-03 00:01:37 +0800

    seed file updated

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,35 +6,41 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-students = [
-  {first_name: "Ellen", last_name: "Lau"},
-  {first_name: "Bennis", last_name: "Yiu"},
-  {first_name: "Spencer", last_name: "Cheung"},
-  {first_name: "Vanessa", last_name: "Auyeung"},
-  {first_name: "Billy", last_name: "Wong"},
-]
+# students = [
+#   { first_name: "Ellen", last_name: "Lau" },
+#   { first_name: "Bennis", last_name: "Yiu"} ,
+#   { first_name: "Spencer", last_name: "Cheung" },
+#   { first_name: "Vanessa", last_name: "Auyeung" },
+#   { first_name: "Billy", last_name: "Wong" },
+# ]
 
-Student.create!(students)
+# Student.create!(students)
+# puts "#{students.length()} students have been created!"
 
-teachers = [
-  {first_name: "Kelvin", last_name: "Chan", major: "Computer Science"},
-  {first_name: "Mark", last_name: "Leung", major: "Engineering"},
-  {first_name: "Queena", last_name: "Ling", major: "Education"},
-  {first_name: "Tommie", last_name: "Lo", major: "Economics"},
-  {first_name: "Kenneth", last_name: "She", major: "Computer Science"},
-]
+# teachers = [
+#   { first_name: "Kelvin", last_name: "Chan", major: "Computer Science" },
+#   { first_name: "Mark", last_name: "Leung", major: "Engineering" },
+#   { first_name: "Queena", last_name: "Ling", major: "Education" },
+#   { first_name: "Tommie", last_name: "Lo", major: "Economics" },
+#   { first_name: "Kenneth", last_name: "She", major: "Computer Science" },
+# ]
 
-Teacher.create!(teachers)
+# Teacher.create!(teachers)
+# puts "#{teachers.length()} teachers have been created!"
 
-names = ["Scratch", "ScratchJr", "App Inventor", "Front End Programming",  "Python", "Minecraft"]
-days = ["Monday", "Tuesday", "Friday", "Saturday", "Sunday"]
+# names = ["Scratch", "ScratchJr", "App Inventor", "Front End Programming",  "Python", "Minecraft"]
+# days = ["Monday", "Tuesday", "Friday", "Saturday", "Sunday"]
 
-courses = [
-  {name: names.sample, day: days.sample, student_id: rand(1..6), teacher_id: rand(1..6)},
-  {name: names.sample, day: days.sample, student_id: rand(1..6), teacher_id: rand(1..6)},
-  {name: names.sample, day: days.sample, student_id: rand(1..6), teacher_id: rand(1..6)},
-  {name: names.sample, day: days.sample, student_id: rand(1..6), teacher_id: rand(1..6)},
-  {name: names.sample, day: days.sample, student_id: rand(1..6), teacher_id: rand(1..6)}
-]
+# stud_id_arr = Student.all.pluck(:id)
+# t_id_arr = Teacher.all.pluck(:id)
 
-Course.create!(courses)
+# courses = [
+#   { name: names.sample, day: days.sample, student_id: stud_id_arr.sample, teacher_id: t_id_arr.sample },
+#   { name: names.sample, day: days.sample, student_id: stud_id_arr.sample, teacher_id: t_id_arr.sample },
+#   { name: names.sample, day: days.sample, student_id: stud_id_arr.sample, teacher_id: t_id_arr.sample },
+#   { name: names.sample, day: days.sample, student_id: stud_id_arr.sample, teacher_id: t_id_arr.sample },
+#   { name: names.sample, day: days.sample, student_id: stud_id_arr.sample, teacher_id: t_id_arr.sample },
+# ]
+
+# Course.create!(courses)
+# puts "#{courses.length()} courses have been created!"
